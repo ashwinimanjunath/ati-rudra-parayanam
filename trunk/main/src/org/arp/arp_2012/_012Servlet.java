8819c53a2b0bbd4f87c2b8d3880e2e1d01f231da
raghuram.kanadam@gmail.com <raghuram.kanadam@gmail.com@c942dbdf-e558-9abe-98b9-b04406a7e3a4>
2012-01-07 03:58:01 +0000

    App engine here we come!

---
+++ trunk/main/src/org/arp/arp_2012/_012Servlet.java
@@ -0,0 +1,13 @@
+package org.arp.arp_2012;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class _012Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
