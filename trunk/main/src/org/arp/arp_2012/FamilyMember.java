1105788f040efbe04002794b1714bc9f4e2250ef
ashwini.twinkle@gmail.com <ashwini.twinkle@gmail.com@c942dbdf-e558-9abe-98b9-b04406a7e3a4>
2012-01-15 22:51:47 +0000

    Adding family members

---
+++ trunk/main/src/org/arp/arp_2012/FamilyMember.java
@@ -0,0 +1,22 @@
+package org.arp.arp_2012;
+
+public class FamilyMember {
+	private String name;
+	private String relationship;
+
+	public final String getName() {
+		return name;
+	}
+
+	public final String getRelationship() {
+		return relationship;
+	}
+
+	public final void setName(String name) {
+		this.name = name;
+	}
+
+	public final void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+}
